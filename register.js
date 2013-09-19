8bd6c00575b5a587c0aa71c2f22d12149d993834
mbove <mbove@andrew.cmu.edu>
2013-09-18 23:58:53 -0400

    added form validation

--- register.js
+++ register.js
@@ -1,3 +1,14 @@
+function ticketValidate(){
+	var ticket_num = $('[name="ticket_num"]').val();
+	ticket_num = parseInt(ticket_num);
+	if(ticket_num){
+		if(ticket_num>0 && ticket_num<999){
+			return true;
+		}
+	}
+	return false;
+}
+
 $(document).ready(function(){
 	$( ".games" ).sortable();
     $( ".games" ).disableSelection();
