6cbe1f7bc30bcba79ec6d366deba2ddaddb90c98
mbove <mbove@andrew.cmu.edu>
2013-09-19 00:34:20 -0400

    added form alert in register.html

--- register.js
+++ register.js
@@ -3,13 +3,16 @@ function ticketValidate(){
 	ticket_num = parseInt(ticket_num);
 	if(ticket_num){
 		if(ticket_num>0 && ticket_num<999){
-			return true;
+			var ticket_code = $('[name="ticket_code"]').val();
+			if(ticket_code.length==5){
+				return true;
+			}
 		}
 	}
+	$('.directions.first').after("<div class='alert'>Invalid Ticket Number or Code</div>");
+	$('input[type="text"]').css('border-color', 'red');
+	setInterval(function(){
+		$('.alert').hide();
+	}, 5000);
 	return false;
 }
-
-$(document).ready(function(){
-	$( ".games" ).sortable();
-    $( ".games" ).disableSelection();
-});
