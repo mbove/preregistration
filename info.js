b30d39d853520900ee3e88a80600b12c21c8e448
mbove <mbove@andrew.cmu.edu>
2013-09-19 23:22:29 -0400

    added form validations to info.js

--- info.js
+++ info.js
@@ -1,20 +1,32 @@
 function infoValidate(){
-	var ticket_num = $('[name="ticket_num"]').val();
-	ticket_num = parseInt(ticket_num);
-	if(ticket_num){
-		if(ticket_num>0 && ticket_num<999){
-			var ticket_code = $('[name="ticket_code"]').val();
-			if(ticket_code.length==5){
-				return true;
-			}
-		}
+	var last_name = $('[name="last_name"]').val();
+	var first_name = $('[name="first_name"]').val();
+	var phone = $('[name="phone"]').val();
+	var email = $('[name="email"]').val();
+	var interval;
+	$('[type="text"]').css('border', '1px solid gray');
+	$('.alert').remove();
+	if(!first_name || !last_name || !phone || !email){
+		$('.directions.first').after("<div class='alert'>Please fill in all information</div>");
+		if(!first_name){ $('input[name="first_name"]').css('border-color', 'red'); }
+		if(!last_name){ $('input[name="last_name"]').css('border-color', 'red'); }
+		if(!phone){ $('input[name="phone"]').css('border-color', 'red'); }
+		if(!email){ $('input[name="email"]').css('border-color', 'red');}
+		interval = setInterval(function(){
+			$('.alert').hide();
+		}, 5000);
+		$('html, body').animate({ scrollTop: 0 }, 'fast');
+		return false;
 	}
-	$('.directions.first').after("<div class='alert'>Invalid Ticket Number or Code</div>");
-	$('input[type="text"]').css('border-color', 'red');
-	setInterval(function(){
-		$('.alert').hide();
-	}, 5000);
-	return false;
+	if(phone.length < 10){
+		$('.directions.first').after("<div class='alert'>Invalid Phone Number</div>");
+		interval = setInterval(function(){
+			$('.alert').hide();
+		}, 5000);
+		$('html, body').animate({ scrollTop: 0 }, 'fast');
+		return false;
+	}
+	return true;
 }
 $(document).ready(function(){
 	$( ".games" ).sortable();
--- register.js
+++ register.js
@@ -2,12 +2,10 @@ function ticketValidate(){
 	var ticket_num = $('[name="ticket_num"]').val();
 	ticket_num = parseInt(ticket_num);
 	var ticket_code = $('[name="ticket_code"]').val();
-	var msg = '';
 	$('[name="ticket_num"]').css('border', '1px solid gray');
 	$('[name="ticket_code"]').css('border', '1px solid gray');
 	$('.alert').remove();
 	if(!ticket_num || !ticket_code){
-		$()
 		$('.directions.first').after("<div class='alert'>Please fill out both fields</div>");
 		if(!ticket_num){
 			$('[name="ticket_num"]').css('border-color', 'red');
