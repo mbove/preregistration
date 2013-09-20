5ec5815f6c3441b7a850e35f4d8961901236c629
mbove <mbove@andrew.cmu.edu>
2013-09-19 22:49:50 -0400

    fixed javascript validation for the first registration page

--- info.js
+++ info.js
@@ -1,3 +1,21 @@
+function infoValidate(){
+	var ticket_num = $('[name="ticket_num"]').val();
+	ticket_num = parseInt(ticket_num);
+	if(ticket_num){
+		if(ticket_num>0 && ticket_num<999){
+			var ticket_code = $('[name="ticket_code"]').val();
+			if(ticket_code.length==5){
+				return true;
+			}
+		}
+	}
+	$('.directions.first').after("<div class='alert'>Invalid Ticket Number or Code</div>");
+	$('input[type="text"]').css('border-color', 'red');
+	setInterval(function(){
+		$('.alert').hide();
+	}, 5000);
+	return false;
+}
 $(document).ready(function(){
 	$( ".games" ).sortable();
     $( ".games" ).disableSelection();
