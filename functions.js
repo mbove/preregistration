db3d3a3931752bd1ec17583e0dc1ee6bbba0d413
mbove <mbove@andrew.cmu.edu>
2013-09-23 02:24:26 -0400

    moved javascript functions in info and register to functions.ks

--- functions.js
+++ functions.js
@@ -1,3 +1,4 @@
+//Reusable
 function createAlert(message){
 	$('.directions.first').after("<div class='alert'>"+message+"</div>");
 	setInterval(function(){
@@ -7,4 +8,65 @@ function createAlert(message){
 }
 function highlightField(field_name, color){
 	$('[name="'+field_name+'"]').css('border-color', color);
+}
+
+
+//Form Validation
+
+
+function ticketValidate(){
+	var ticket_num = $('[name="ticket_num"]').val();
+	ticket_num = parseInt(ticket_num);
+	var ticket_code = $('[name="ticket_code"]').val();
+	$('[type="text"]').css('border', '1px solid gray');
+	$('.alert').remove();
+	if(!ticket_num || !ticket_code){
+		if(!ticket_num){
+			highlightField("ticket_num", "red");
+		}		
+		if(!ticket_code){
+			highlightField("ticket_code", "red");
+		}
+		createAlert("Please fill out all fields");
+		return false;
+	}
+	if(validCode(ticket_num, ticket_code)){
+		return true;
+	}
+	else{
+		highlightField("ticket_num", "red");
+		highlightField("ticket_code", "red");
+		createAlert("Invalid Ticket Number or Code");
+		return false;
+	}
+}
+function validCode(ticket_num, ticket_code){
+	if(ticket_num==101 && ticket_code=="qwerty"){
+		//Ticket combo for a regular player
+		return true;
+	}
+	if(ticket_num==999 && ticket_code=="asdfgh"){
+		//ticket combo for a volunteer
+		$('form').attr('action', 'volunteer_info.html');
+		//redirects form field
+		return true;
+	}
+	return false;
+}
+function infoValidate(){
+	var last_name = $('[name="last_name"]').val();
+	var first_name = $('[name="first_name"]').val();
+	var phone = $('[name="phone"]').val();
+	var email = $('[name="email"]').val();
+	var interval;
+	$('[type="text"]').css('border', '1px solid gray');
+	$('.alert').remove();
+	if(!first_name || !last_name || !email){
+		createAlert('Please fill in all information');
+		if(!first_name){ highlightField('first_name', 'red'); }
+		if(!last_name){ highlightField('last_name', 'red'); }
+		if(!email){ highlightField('email', 'red'); }
+		return false;
+	}
+	return true;
 }
--- info.js
+++ info.js
@@ -1,20 +1,3 @@
-function infoValidate(){
-	var last_name = $('[name="last_name"]').val();
-	var first_name = $('[name="first_name"]').val();
-	var phone = $('[name="phone"]').val();
-	var email = $('[name="email"]').val();
-	var interval;
-	$('[type="text"]').css('border', '1px solid gray');
-	$('.alert').remove();
-	if(!first_name || !last_name || !email){
-		createAlert('Please fill in all information');
-		if(!first_name){ highlightField('first_name', 'red'); }
-		if(!last_name){ highlightField('last_name', 'red'); }
-		if(!email){ highlightField('email', 'red'); }
-		return false;
-	}
-	return true;
-}
 $(document).ready(function(){
 	$( ".games" ).sortable();
     $( ".games" ).disableSelection();
