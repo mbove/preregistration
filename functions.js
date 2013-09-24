d5186c1b1b70f72994b29b698cbdd7204bcebd3f
mbove <mbove@andrew.cmu.edu>
2013-09-23 22:59:31 -0400

    changed spacing in functions.js

--- functions.js
+++ functions.js
@@ -1,15 +1,16 @@
 //Reusable
 var alert;
+
 function createAlert(message){
 	$('.alert').remove();
 	$('.directions.first').after("<div class='alert'>"+message+"</div>");
 	clearInterval(alert);
-	
 	alert = setInterval(function(){
 			$('.alert').remove();
 		}, 5000);
 	$('html, body').animate({ scrollTop: 0 }, 'fast');
 }
+
 function highlightField(field_name, color){
 	$('[name="'+field_name+'"]').css('border-color', color);
 }
